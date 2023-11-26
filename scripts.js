88a25d9e67fa5cecee07fc5c2ebf40e3c6558e0f
Satwik-panda <satwikpanda83@gmail.com>
2023-11-26 21:17:55 +0530

    integrated with local storage

--- scripts.js
+++ scripts.js
@@ -1,17 +1,5 @@
-window.addEventListener('load',()=>{
-    var form=document.querySelector("#task-entry");
-    var input_task=document.querySelector("#new-task");
-    var section=document.querySelector(".tasks");
-
-    form.addEventListener("submit",(e)=>{
-        e.preventDefault();
-
-        var task=input_task.value;
-        if(!task)
-        {
-            alert("please enter some task and then add");
-            return;
-        }
+function addElement(task){
+        var section=document.querySelector(".tasks");
 
         var new_task=document.createElement("div");
         new_task.classList.add("task-data-row");
@@ -23,42 +11,59 @@ window.addEventListener('load',()=>{
         task_content_input.type="text";
         task_content_input.value=task;
         task_content_input.classList.add("text");
-        task_content_input.setAttribute("readonly", "readonly");
 
         var actiondiv=document.createElement("div")
         actiondiv.classList.add("actions");
-        var edit_btn=document.createElement("button");
-        edit_btn.classList.add("edit");
-        edit_btn.innerHTML="Edit";
 
         var del_btn=document.createElement("button");
         del_btn.classList.add("delete");
         del_btn.innerHTML="Delete";
         
-        actiondiv.appendChild(edit_btn);
         actiondiv.appendChild(del_btn);
         task_content.appendChild(task_content_input);
         new_task.appendChild(task_content);
         new_task.appendChild(actiondiv)
         section.appendChild(new_task);
+        updateLocalStorage();
 
-        edit_btn.addEventListener('click',()=>{
-            if(edit_btn.innerHTML==="Edit"){
-                task_content_input.removeAttribute("readonly");
-                task_content_input.focus();
-                edit_btn.innerHTML="Save";
-            }
-            else{
-                task_content_input.setAttribute("readonly", "readonly");
-                edit_btn.innerHTML="Edit";
-            }
-        })
-        
         del_btn.addEventListener("click",()=>{
             section.removeChild(new_task);
+            updateLocalStorage();
         })
 
+        var input_task=document.querySelector("#new-task");
         input_task.value="";//to clear the add task box;
-        
+
+}
+
+window.addEventListener('load',()=>{
+    //populate previously stored data
+    console.log(JSON.parse(localStorage.getItem('tasks')));
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const taskList = document.querySelector(".task-data-row");
+    savedTasks.forEach(task => {
+            addElement(task);
+        });
+
+    var form=document.querySelector("#task-entry");
+    var input_task=document.querySelector("#new-task");
+    
+    //add new task row
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        var task=input_task.value;
+        if(!task)
+        {
+            alert("please enter some task and then add");
+            return;
+        }
+        addElement(task);
     })
 })
+
+function updateLocalStorage(){
+    const allTaskRows= document.querySelectorAll(".text");
+    const taskarr= Array.from(allTaskRows);
+    const taskTexts = taskarr.map(task => task.value);
+    localStorage.setItem('tasks', JSON.stringify(taskTexts));
+}
